db5eee69098593b793df593819ca7db714d3a818
Sacha Benda <sacha.benda@gmail.com>
2013-09-13 17:40:56 +0200

    Implemented Isotopo-Shortcode-Plugin

--- js/custom.js
+++ js/custom.js
@@ -1,14 +1,4 @@
 jQuery(document).ready(function($){
 
-	//Masonry settings
-	  //$('#main').masonry({ singleMode: true });
-
-	//Isotope Settings
-	 $('.moveplugins_isotopes_container').isotope('reLayout');//{
-	//   	masonry: {
-	//     	columnWidth: 110,
-	//     	gutterWidth: 10
-	//   	}
-	// });
 
 });
---
+++ template-homepage.php
@@ -0,0 +1,41 @@
+<?php
+/*
+ * Template Name: Project Homepage
+ *
+ *
+ * @package AgoBenda
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		<?php if ( have_posts() ) : ?>
+
+			<?php /* Start the Loop */ ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php
+					/* Wi are calling the plugin shortcode to fill the page.
+					 * No content is 
+					 * If you want to override this in a child theme, then include a file
+					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+					 */
+					echo do_shortcode( '[isotope-posts]' );
+				?>
+
+			<?php endwhile; ?>
+
+			<?php agobenda_content_nav( 'nav-below' ); ?>
+
+		<?php else : ?>
+
+			<?php get_template_part( 'no-results', 'index' ); ?>
+
+		<?php endif; ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
