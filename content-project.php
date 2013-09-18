56ed6f90a0120577b0b7b09f43c91b26b0824149
Sacha Benda <sacha.benda@gmail.com>
2013-09-18 18:52:16 +0200

    List of attached images in project post

--- content-project.php
+++ content-project.php
@@ -6,14 +6,85 @@
  */
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class('xxx'); ?>>
-	<div class="entry-content">
-		<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'agobenda' ) ); ?>
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . __( 'Pages:', 'agobenda' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<div class="post-container">
+		<header class="entry-header">
+			<h1 class="entry-title"><?php the_title(); ?></h1>
+
+			<div class="entry-meta">
+				<?php agobenda_posted_on(); ?>
+			</div><!-- .entry-meta -->
+		</header><!-- .entry-header -->
+
+		<div class="entry-content">
+			<?php the_content(); ?>
+<div>
+
+</div>			
+			<?php
+				wp_link_pages( array(
+					'before' => '<div class="page-links">' . __( 'Pages:', 'agobenda' ),
+					'after'  => '</div>',
+				) );
+			?>
+		</div><!-- .entry-content -->
+
+		<footer class="entry-meta">
+			<?php
+				/* translators: used between list items, there is a space after the comma */
+				$category_list = get_the_category_list( __( ', ', 'agobenda' ) );
+
+				/* translators: used between list items, there is a space after the comma */
+				$tag_list = get_the_tag_list( '', __( ', ', 'agobenda' ) );
+
+				if ( ! agobenda_categorized_blog() ) {
+					// This blog only has 1 category so we just need to worry about tags in the meta text
+					if ( '' != $tag_list ) {
+						$meta_text = __( 'This entry was tagged %2$s. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'agobenda' );
+					} else {
+						$meta_text = __( 'Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'agobenda' );
+					}
+
+				} else {
+					// But this blog has loads of categories so we should probably display them here
+					if ( '' != $tag_list ) {
+						$meta_text = __( 'This entry was posted in %1$s and tagged %2$s. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'agobenda' );
+					} else {
+						$meta_text = __( 'This entry was posted in %1$s. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'agobenda' );
+					}
+
+				} // end check for categories on this blog
+
+				printf(
+					$meta_text,
+					$category_list,
+					$tag_list,
+					get_permalink(),
+					the_title_attribute( 'echo=0' )
+				);
+			?>
+
+			<?php edit_post_link( __( 'Edit', 'agobenda' ), '<span class="edit-link">', '</span>' ); ?>
+		</footer><!-- .entry-meta -->
+	</div> <!-- post-container -->
+	<div class="post-images">
+<?php //if ( $post->post_type == 'project' && $post->post_status == 'publish' ) {
+		$attachments = get_posts( array(
+			'post_type' => 'attachment',
+			'posts_per_page' => -1,
+			'post_parent' => $post->ID,
+			'exclude'     => get_post_thumbnail_id()
+		) );
+
+		if ( $attachments ) {
+			foreach ( $attachments as $attachment ) {
+				$class = "post-attachment mime-" . sanitize_title( $attachment->post_mime_type );
+				$thumbimg = wp_get_attachment_link( $attachment->ID, 'thumbnail-size', true );
+				echo '<li class="' . $class . ' project-thumbnail">' . $thumbimg . '</li>';
+			}
+			
+		}
+	//}
+?>
+	</div>
 </article><!-- #post-## -->
