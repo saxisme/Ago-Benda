ad30cd07ed9cabdad4bf86b24a40b4a09f655796
Sacha Benda <sacha.benda@gmail.com>
2013-09-25 15:54:14 +0200

    favico

--- header.php
+++ header.php
@@ -9,6 +9,7 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
+<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title><?php wp_title( '|', true, 'right' ); ?></title>
