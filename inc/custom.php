31325bb6b1103ef6eb73475e1fca11ed20dc0318
Sacha Benda <sacha.benda@gmail.com>
2013-09-25 16:08:52 +0200

    Google Analytics for logged out users

--- inc/custom.php
+++ inc/custom.php
@@ -312,3 +312,27 @@ function sax_callme( $atts, $content = null ) {
      return '<div class="callme-container"><img src="' . get_stylesheet_directory_uri() .'/images/phone.png" /></div>';
 }
 add_shortcode( 'callme', 'sax_callme' );
+
+// Include the Google Analytics Tracking Code (ga.js)
+// @ http://code.google.com/apis/analytics/docs/tracking/asyncUsageGuide.html
+function google_analytics_tracking_code(){
+
+	$propertyID = 'UA-44340447-1'; // GA Property ID
+	if ( !is_user_logged_in() ) {
+		//if ($options['ga_enable']) { ?>
+		<script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		  ga('create', '<?php echo $propertyID ?>', 'agobenda.com');
+		  ga('send', 'pageview');
+
+		</script>
+	<?php
+	}
+}
+
+// include GA tracking code before the closing head tag
+add_action('wp_head', 'google_analytics_tracking_code');
