39214953d577bb76fd89ef1e244c4b56dbcc96c0
Sacha Benda <sacha.benda@gmail.com>
2013-09-30 14:04:39 +0200

    tiled test

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@
  * Set the content width based on the theme's design and stylesheet.
  */
 if ( ! isset( $content_width ) )
-	$content_width = 250; /* pixels */
+	$content_width = 577; /* pixels */
 
 if ( ! function_exists( 'agobenda_setup' ) ) :
 /**
