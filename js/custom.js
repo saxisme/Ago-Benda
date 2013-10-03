05fe9ea1b856494b3a5b901c40a49ff503af95d8
Sacha Benda <sacha.benda@gmail.com>
2013-10-03 13:50:09 +0200

    Contact Placeholder

--- js/custom.js
+++ js/custom.js
@@ -7,17 +7,6 @@ jQuery(document).ready(function($){
 	jQuery('.contact-form input.url').attr('placeholder', 'Website');
 	jQuery('.contact-form textarea').attr('placeholder', 'Comment*');
 
-	// Fixing Webkit that not clearing input/textarea when get focus
-	$(function(){
-	  //if ($.browser.webkit) {
-	    $('input, textarea').on('focus',function(){
-	      if ( $(this).attr('placeholder') ) $(this).data('placeholder', $(this).attr('placeholder')).removeAttr('placeholder');
-	}).on('blur', function(){
-	        if ( $(this).data('placeholder') ) $(this).attr('placeholder', $(this).data('placeholder')).removeData('placeholder');
-	    });
-	  //}
-	});
-
 });
 
 //Smooth Back to Top Effect
