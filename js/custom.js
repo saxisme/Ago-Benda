14169f313c82303629342e579d4474723dc21fd5
Sacha Benda <sacha.benda@gmail.com>
2013-10-04 17:39:23 +0200

    css video fix

--- js/custom.js
+++ js/custom.js
@@ -44,3 +44,39 @@ jQuery(document).ready(function(){
     //Swipebox
     jQuery(".swipebox").swipebox();
 });
+
+//Video
+jQuery(document).ready(function(){
+    //Fluid Width Video
+    //http://css-tricks.com/NetMag/FluidWidthVideo/demo.php
+    $(function() {
+        
+        var $allVideos = $("iframe[src^='http://player.vimeo.com'], iframe[src^='http://www.youtube.com'], object, embed"),
+        $fluidEl = $("figure");
+                
+        $allVideos.each(function() {
+        
+          $(this)
+            // jQuery .data does not work on object/embed elements
+            .attr('data-aspectRatio', this.height / this.width)
+            .removeAttr('height')
+            .removeAttr('width');
+        
+        });
+        
+        $(window).resize(function() {
+        
+          var newWidth = $fluidEl.width();
+          $allVideos.each(function() {
+          
+            var $el = $(this);
+            $el
+                .width(newWidth)
+                .height(newWidth * $el.attr('data-aspectRatio'));
+          
+          });
+        
+        }).resize();
+
+    });
+});
